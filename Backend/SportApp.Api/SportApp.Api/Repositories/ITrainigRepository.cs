5c7f7ad3f56245c172302d666385d3be1ce674aa
Wawrzyniec <wawrzyniec.korzen@gmail.com>
2026-08-29 16:31:23 +0200

    added interface and repository for CRUD operations for Trainings

---
+++ Backend/SportApp.Api/SportApp.Api/Repositories/ITrainigRepository.cs
@@ -0,0 +1,13 @@
+﻿using SportApp.Api.Models;
+
+namespace SportApp.Api.Repositories
+{
+    public interface ITrainigRepository
+    {
+        Task<IEnumerable<Training>> GetAllAsync();
+        Task<Training?> GetByIdAsync(int id);
+        Task<Training> AddAsync(Training training);
+        Task UpdateAsync(Training training);
+        Task DeleteAsync(Training training);
+    }
+}
---
+++ Backend/SportApp.Api/SportApp.Api/Repositories/TrainingRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportApp.Api.Data;
+using SportApp.Api.Models;
+
+namespace SportApp.Api.Repositories
+{
+    public class TrainingRepository :ITrainigRepository
+    {
+        private readonly AppDbContext _context;
+
+        public TrainingRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Training>> GetAllAsync()
+        {
+            return await _context.Trainings.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Training?> GetByIdAsync(int id)
+        {
+            return await _context.Trainings.AsNoTracking().FirstOrDefaultAsync(t => t.Id == id);
+        }
+
+        public async Task<Training> AddAsync(Training training)
+        {
+            await _context.Trainings.AddAsync(training);
+            await _context.SaveChangesAsync();
+            return training;
+        }
+
+        public async Task UpdateAsync(Training training)
+        {
+            _context.Trainings.Update(training);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Training training)
+        {
+            _context.Trainings.Remove(training);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
