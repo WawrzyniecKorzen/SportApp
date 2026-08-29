da66ce7b6055a8fcf43a6c2ed2b369aeaf5ffc5a
Wawrzyniec <wawrzyniec.korzen@gmail.com>
2026-08-29 16:47:59 +0200

    Added Service Layer interface and Service to handle CRUD for Trainings

---
+++ Backend/SportApp.Api/SportApp.Api/Services/ITrainingService.cs
@@ -0,0 +1,16 @@
+﻿using SportApp.Api.Models;
+
+namespace SportApp.Api.Services
+{
+    public interface ITrainingService
+    {
+        Task<IEnumerable<Training>> GetAllAsync();
+
+        Task<Training> GetByIdAsync(int id);
+        Task<Training> AddAsync(Training training);
+
+        Task<bool> UpdateAsync(int id, Training training);
+
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ Backend/SportApp.Api/SportApp.Api/Services/TrainingService.cs
@@ -0,0 +1,57 @@
+﻿using SportApp.Api.Models;
+using SportApp.Api.Repositories;
+
+namespace SportApp.Api.Services
+{
+    public class TrainingService : ITrainingService
+    {
+        private readonly ITrainigRepository _repository;
+
+        public TrainingService(ITrainigRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<IEnumerable<Training>> GetAllAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<Training> GetByIdAsync(int id)
+        {
+            return await _repository.GetByIdAsync(id);
+        }
+
+        public Task<Training> AddAsync(Training training)
+        {
+            return _repository.AddAsync(training);
+        }
+
+        public async Task<bool> UpdateAsync(int id, Training training)
+        {
+            var existingTraining = await _repository.GetByIdAsync(id);
+            if (existingTraining == null) return false;
+
+            existingTraining.Type = training.Type;
+            existingTraining.Date = training.Date;
+            existingTraining.Duration = training.Duration;
+            existingTraining.Distance = training.Distance;
+            existingTraining.Calories = training.Calories;
+            existingTraining.Description = training.Description;
+
+            await _repository.UpdateAsync(existingTraining);
+
+            return true;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var training = await _repository.GetByIdAsync(id);
+
+            if (training == null) return false;
+
+            await _repository.DeleteAsync(training);
+
+            return true;
+        }
+    }
+}
