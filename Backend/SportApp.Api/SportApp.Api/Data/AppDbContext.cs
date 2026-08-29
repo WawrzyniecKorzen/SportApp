faf1610d78dc2ba2195201c5d9529cc409f6a20f
Wawrzyniec <wawrzyniec.korzen@gmail.com>
2026-08-29 13:40:58 +0200

    Added AppDbContext to create Db using Model - included manual function to set relation between User and Training

---
+++ Backend/SportApp.Api/SportApp.Api/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportApp.Api.Models;
+
+namespace SportApp.Api.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Training> Trainings { get; set; }
+
+       //recznie buduje relacje miedzy User i Training (choc powinno sie tez zrobic automatycznie)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Training>()
+                .HasOne(t => t.User)
+                .WithMany(u => u.Trainings)
+                .HasForeignKey(t => t.User.Id)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
