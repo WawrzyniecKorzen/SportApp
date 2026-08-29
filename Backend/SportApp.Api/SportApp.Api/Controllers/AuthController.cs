aa039d2ca832aa59017a57989c7a0225a451e3a1
Wawrzyniec <wawrzyniec.korzen@gmail.com>
2026-08-29 20:14:36 +0200

    Added AuthController

---
+++ Backend/SportApp.Api/SportApp.Api/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportApp.Api.DTOs.Auth;
+using SportApp.Api.Services;
+
+namespace SportApp.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController :ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AuthResponse>> Register(RegisterRequest request)
+        {
+            try
+            {
+                var response = await _authService.RegisterAsync(request);
+                return Ok(response);
+            }
+            catch (InvalidOperationException e)
+            {
+                return Conflict(new { message = e.Message });
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthResponse>> Login(LoginRequest request)
+        {
+            var response = await _authService.LoginAsync(request);
+
+            if (response == null) return Unauthorized( new { message = "Wrong email or password"});
+
+            return Ok(response);
+        }
+    }
+}
