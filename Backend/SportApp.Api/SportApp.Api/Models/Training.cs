5918b9fa32b9568fcc2ffacd5f49582c0f243c4e
Wawrzyniec <wawrzyniec.korzen@gmail.com>
2026-08-29 13:26:03 +0200

    Added Model folder with two model classes: User and Training

---
+++ Backend/SportApp.Api/SportApp.Api/Models/Training.cs
@@ -0,0 +1,18 @@
+﻿namespace SportApp.Api.Models
+{
+    public class Training
+    {
+        public int Id { get; set; }
+        public int USerId { get; set; }
+        public string Type { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public int Duration {  get; set; }
+        public double Distance { get; set; }
+        public int Calories { get; set; }
+        public string? Description { get; set; } // bo opcjonalny
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        public User User { get; set; }
+
+    }
+}
---
+++ Backend/SportApp.Api/SportApp.Api/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace SportApp.Api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public ICollection<Training> Trainings { get; set; } = new List<Training>();
+
+    }
+}
