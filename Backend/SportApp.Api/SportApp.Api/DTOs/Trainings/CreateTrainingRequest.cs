60ddf766a38443cac677d3059422c93797912d9a
Wawrzyniec <wawrzyniec.korzen@gmail.com>
2026-09-01 20:26:53 +0200

    Added validation for trainings and validation constants

--- Backend/SportApp.Api/SportApp.Api/DTOs/Trainings/CreateTrainingRequest.cs
+++ Backend/SportApp.Api/SportApp.Api/DTOs/Trainings/CreateTrainingRequest.cs
@@ -1,4 +1,6 @@
-﻿using SportApp.Api.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using SportApp.Api.Models;
+using SportApp.Api.Validation;
 
 namespace SportApp.Api.DTOs.Trainings
 {
@@ -6,9 +8,17 @@ namespace SportApp.Api.DTOs.Trainings
     {
         public TrainingType Type { get; set; }
         public DateTime Date { get; set; }
+
+        [Range(ValidationConstants.MinDurationMinutes, ValidationConstants.MaxDurationMinutes)]
         public int Duration { get; set; }
+
+        [Range(0, ValidationConstants.MaxDistanceKm)]
         public double Distance { get; set; }
+
+        [Range(0, ValidationConstants.MaxCalories)]
         public int Calories { get; set; }
+
+        [StringLength(ValidationConstants.MaxDescriptionLength)]
         public string? Description { get; set; }
     }
 }
---
+++ Backend/SportApp.Api/SportApp.Api/Validation/ValidationConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace SportApp.Api.Validation
+{
+    public class ValidationConstants
+    {
+        public const int MaxDurationMinutes = 1440;
+        public const int MinDurationMinutes = 1;
+        public const double MaxDistanceKm = 1000;
+        public const int MaxCalories = 100000;
+        public const int MaxDescriptionLength = 1000;
+    }
+}
