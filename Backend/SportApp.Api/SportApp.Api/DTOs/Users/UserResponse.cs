55c3734bbfd10c973b33c498656f2144c9492bca
Wawrzyniec <wawrzyniec.korzen@gmail.com>
2026-09-05 16:06:59 +0200

    Added User Response in DTOs for communicating User data

---
+++ Backend/SportApp.Api/SportApp.Api/DTOs/Users/UserResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace SportApp.Api.DTOs.Users
+{
+    public class UserResponse
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+    }
+}
