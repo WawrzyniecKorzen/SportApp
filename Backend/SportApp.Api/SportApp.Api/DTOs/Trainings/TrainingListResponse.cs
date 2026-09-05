9fdeb41840bdfb585849c1f4d343303e6814b758
Wawrzyniec <wawrzyniec.korzen@gmail.com>
2026-09-05 12:47:04 +0200

    Added DTOs to handle queries of trainings and response lists

---
+++ Backend/SportApp.Api/SportApp.Api/DTOs/Trainings/TrainingListResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace SportApp.Api.DTOs.Trainings
+{
+    public class TrainingListResponse
+    {
+        public IEnumerable<TrainingResponse> Items { get; set; } = new List<TrainingResponse>();
+
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
---
+++ Backend/SportApp.Api/SportApp.Api/DTOs/Trainings/TrainingQueryParameters.cs
@@ -0,0 +1,13 @@
+﻿using SportApp.Api.Models;
+
+namespace SportApp.Api.DTOs.Trainings
+{
+    public class TrainingQueryParameters
+    {
+        public DateTime? From { get; set; }
+        public DateTime? To { get; set; }
+        public TrainingType? Type { get; set; }
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 20;
+    }
+}
