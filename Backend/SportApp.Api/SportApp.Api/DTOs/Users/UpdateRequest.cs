7c777bffd8b0a11938d1d3e37420906d43c6f6ca
Wawrzyniec <wawrzyniec.korzen@gmail.com>
2026-09-05 16:45:31 +0200

    Added UpdateUserRequest DTO.

---
+++ Backend/SportApp.Api/SportApp.Api/DTOs/Users/UpdateRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportApp.Api.DTOs.Users
+{
+    public class UpdateUserRequest
+    {
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+    }
+}
