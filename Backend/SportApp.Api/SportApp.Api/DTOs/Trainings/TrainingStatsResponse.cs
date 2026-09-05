a4c0b543d221fb91ec391f2d774df2639b16f063
Wawrzyniec <wawrzyniec.korzen@gmail.com>
2026-09-05 14:42:51 +0200

    Added DTOs for Training stats: general and specific to given TrainingType

---
+++ Backend/SportApp.Api/SportApp.Api/DTOs/Trainings/TrainingStatsResponse.cs
@@ -0,0 +1,24 @@
+﻿using SportApp.Api.Models;
+
+namespace SportApp.Api.DTOs.Trainings
+{
+    public class TrainingStatsResponse
+    {
+        public int TotalTrainings { get; set; }
+        public int TotalDuration { get; set; }
+        public double TotalDistance { get; set; }
+        public int TotalCalories { get; set; }
+
+        public IEnumerable<TrainingTypeStatsResponse> ByType { get; set; }
+            = new List<TrainingTypeStatsResponse>();
+    }
+
+    public class TrainingTypeStatsResponse
+    {
+        public TrainingType Type { get; set; }
+        public int TrainingsCount { get; set; }
+        public int TotalDuration { get; set; }
+        public double TotalDistance { get; set; }
+        public int TotalCalories { get; set; }
+    }
+}
